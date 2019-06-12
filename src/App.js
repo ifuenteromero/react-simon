d16a243fcd803cc2411dd4be67fe78c6cb04364d
Irene Fuente <ifuenteromero@gmail.com>
2019-06-12 17:47:06 +0200

    order colors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <main className="page">
-      <div className="square-ext red">
-      </div>
-      <div className="square-ext blue">
-      </div>
-      <div className="square-ext yellow">
-      </div>
-      <div className="square-ext green">
-      </div>
-    </main >
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+  handleClick(event) {
+    console.log(event.currentTarget.id)
+  }
+
+  render() {
+    return (
+      <main className="page">
+        <div
+          className="square-ext red"
+          onClick={this.handleClick}
+          id={1}
+
+        >
+        </div>
+        <div
+          className="square-ext blue"
+          onClick={this.handleClick}
+          id={2}
+        >
+        </div>
+        <div
+          className="square-ext yellow"
+          onClick={this.handleClick}
+          id={3}
+        >
+        </div>
+         <div
+          className="square-ext green"
+          onClick={this.handleClick}
+          id={4}
+        >
+        </div>
+      </main >
+    );
+  }
 }
 
 export default App;
